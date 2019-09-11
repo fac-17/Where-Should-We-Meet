960c99633b63541f28a5518c3701ab22c2603cc8
sarah <sarahkharraz@gmail.com>
2019-09-11 15:14:04 +0100

    Create mouse/touch event listener

--- public/js/venuesSwipe.js
+++ public/js/venuesSwipe.js
@@ -1,1 +1,14 @@
-const swipeContainer = document;
+const swipeContainer = document.querySelector(".swipe-container");
+const childCount = swipeContainer.children.length;
+
+swipeContainer.style.setProperty("--n", childCount);
+
+const lock = event => {};
+
+const move = event => {};
+
+swipeContainer.addEventListener("mousedown", lock, false);
+swipeContainer.addEventListener("mouseup", lock, false);
+
+swipeContainer.addEventListener("touchstart", move, false);
+swipeContainer.addEventListener("touchend", move, false);
