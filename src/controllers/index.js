4f44ff693ad83dd2ed41debadbca444d50058dbb
sarah <sarahkharraz@gmail.com>
2019-09-11 12:38:17 +0100

    SetUp files/folders for render swipe gesture

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 //write routes here
+
+router.get("/venues", (req, res) => {
+  res.render("venues", {
+    title: "venues",
+    cssPath: "/css/venuesSwipe.css",
+    jsPath: "/js/venuesSwipe.js"
+  });
+});
 module.exports = router;
