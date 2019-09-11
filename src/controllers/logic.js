5b9d2f756354da6b3dacfce151d61c1ecce5fc67
reubengt <reubengeorgethomas@gmail.com>
2019-09-11 11:20:13 +0100

    rewrite request pure function to use promises, use promise.all to handle parallel api requests

--- src/controllers/logic.js
+++ src/controllers/logic.js
@@ -1,55 +1,35 @@
 //import postcodes from form(when we have it)
-const apiRequest = require("./../request.js");
+const apiRequestPromise = require("./../request.js");
 const geolib = require("geolib");
 
 const postcodeA = "N43HR";
 const postcodeB = "W42LJ";
 let resultArray;
+let centerCoords;
+
 const convertPostcode = postcode => {
-  return whenTaskDone =>
-    apiRequest(`https://api.postcodes.io/postcodes/${postcode}`, (err, res) => {
-      if (err) {
-        whenTaskDone(err);
-      } else {
-        const responseObj = JSON.parse(res);
+  return new Promise((resolve, reject) => {
+    apiRequestPromise(`https://api.postcodes.io/postcodes/${postcode}`)
+      .then(responseFromAPI => {
+        const responseObj = JSON.parse(responseFromAPI);
         const longitude = responseObj.body.result.longitude;
         const latitude = responseObj.body.result.latitude;
         const coords = { longitude, latitude };
-        whenTaskDone(null, coords);
-      }
-    });
-};
-
-const postcodefuncA = convertPostcode(postcodeA);
-const postcodefuncB = convertPostcode(postcodeB);
-const tasksArr = [postcodefuncA, postcodefuncB];
-
-const parallelCoordinateFinder = (tasks, whenAllTasksFinished) => {
-  let counter = 0;
-  let resultArray = [];
-  tasks.forEach((task, i) => {
-    task((err, res) => {
-      //the following code is only run when the response from our api is ready
-      //if successful res would be our coords object
-      counter += 1;
-      if (err) {
-        whenAllTasksFinished(err);
-        return;
-      }
-      resultArray[i] = res;
-      if (counter === tasksArr.length) {
-        whenAllTasksFinished(null, resultArray);
-      }
-    });
+        resolve(coords);
+      })
+      .catch(err => {
+        reject(err);
+      });
   });
 };
 
-parallelCoordinateFinder(tasksArr, (err, resultCoordsArr) => {
-  if (err) {
+const coordsPromiseA = convertPostcode(postcodeA);
+const coordsPromiseB = convertPostcode(postcodeB);
+//use promise.all to get an array of results after both postcode conversion request promises have resolved.
+Promise.all([coordsPromiseA, coordsPromiseB])
+  .then(resultArray => {
+    console.log(resultArray);
+  })
+  .catch(err => {
     console.log(err);
-  } else {
-    console.log(resultCoordsArr);
-  }
-});
-
-// const centerCoords = geolib.getCenterOfBounds(coordsArray);
+  });
--- src/request.js
+++ src/request.js
@@ -1,24 +1,51 @@
 //reusable request module for api calls from backend
 const https = require("https");
 
-const apiRequest = (url, cb) => {
-  https.get(url, responseFromAPI => {
-    let statusCode = responseFromAPI.statusCode;
-    let body = "";
-    responseFromAPI.on("data", chunk => {
-      body += chunk;
-    });
-    responseFromAPI.on("end", () => {
-      let responseObj = {};
-      responseObj.body = JSON.parse(body);
-      responseObj.statusCode = statusCode;
-      if (statusCode !== 200) {
-        cb(new TypeError("StatusCode is not 200, request failed"));
-      } else {
-        cb(null, JSON.stringify(responseObj));
-      }
+const apiRequestPromise = url => {
+  return new Promise((resolve, reject) => {
+    const request = https.get(url, response => {
+      let statusCode = response.statusCode;
+      let body = "";
+      response.on("data", chunk => {
+        body += chunk;
+      });
+      response.on("end", () => {
+        let responseObj = {};
+        responseObj.body = JSON.parse(body);
+        responseObj.statusCode = statusCode;
+        //handle response error
+        if (statusCode !== 200) {
+          reject(
+            new Error(`Request failed with statusCode ${response.statusCode}`)
+          );
+        } else {
+          resolve(JSON.stringify(responseObj));
+        }
+      });
+      //handle connection error
+      request.on("error", err => reject(err));
     });
   });
 };
 
-module.exports = apiRequest;
+// const apiRequest = (url, cb) => {
+//   https.get(url, responseFromAPI => {
+//     let statusCode = responseFromAPI.statusCode;
+//     let body = "";
+//     responseFromAPI.on("data", chunk => {
+//       body += chunk;
+//     });
+//     responseFromAPI.on("end", () => {
+//       let responseObj = {};
+//       responseObj.body = JSON.parse(body);
+//       responseObj.statusCode = statusCode;
+//       if (statusCode !== 200) {
+//         cb(new TypeError("StatusCode is not 200, request failed"));
+//       } else {
+//         cb(null, JSON.stringify(responseObj));
+//       }
+//     });
+//   });
+// };
+
+module.exports = apiRequestPromise;
