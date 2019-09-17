a7fcb582f7c770eeea21fed81018318132d360e6
sarahyjja <44409053+sarahyjja@users.noreply.github.com>
2019-09-17 17:33:26 +0100

    Merge conflicts 2

--- tests/db_test.js
+++ tests/db_test.js
@@ -20,14 +20,10 @@ tape("getData", t => {
         postcodeb: "SW12AA",
         dateinput: "20 October 2019",
         timeinput: "14:50",
-<<<<<<< Updated upstream
-        venuetype: "bar"
-=======
         venuetype: "bar",
         jwtoken: "jwTokenDummy2",
         venuelongitude: null,
         venuelatitude: null
->>>>>>> Stashed changes
       }
     ];
     getData((err, result) => {
@@ -49,10 +45,7 @@ tape("postData", t => {
       "21 August 2020",
       "17.00",
       "restaurant",
-<<<<<<< Updated upstream
-=======
       "webtoken",
->>>>>>> Stashed changes
       (err, res) => {
         if (err) console.log(err);
         getData((err, res) => {
