d3a653f04f513da75fb11901929b0f77227b75c8
sarah <sarahkharraz@gmail.com>
2019-09-17 15:13:46 +0100

    Add stars images

--- src/views/helpers/rating.js
+++ src/views/helpers/rating.js
@@ -1,1 +1,11 @@
-module.exports = number => 
+module.exports = number => {
+  let starsImages = "";
+  let numberOfWholeStars = number.Math.floor(number);
+  for (let i = 0; i < numberOfWholeStars; i++) {
+    starsImages += "<img src='/img/starYellow.svg' class='star'/>";
+  }
+  if (number.toString().includes(".")) {
+    starsImages += "<img src='/img/starGrey.svg' class='star'/>";
+  }
+  return starsImages;
+};
