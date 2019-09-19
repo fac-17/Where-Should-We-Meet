defb84805f0c4f610141bddd0479c08c40582de6
sarah <sarahkharraz@gmail.com>
2019-09-19 17:04:56 +0100

    stars in progress

--- src/views/helpers/starFinder.js
+++ src/views/helpers/starFinder.js
@@ -1,10 +1,17 @@
 module.exports = number => {
   let starsImages = "";
   let numberOfWholeStars = Math.floor(number);
+  const totalOfStars = 5;
+
   for (let i = 0; i < numberOfWholeStars; i++) {
     starsImages += "<img src='/img/starYellow.svg' class='star'/>";
   }
+
   if (number.toString().includes(".")) {
+    starsImages += "<img src='/img/starHalf.svg' class='star'/>";
+  }
+
+  if (starsImages <= totalOfStars) {
     starsImages += "<img src='/img/starGrey.svg' class='star'/>";
   }
   return starsImages;
