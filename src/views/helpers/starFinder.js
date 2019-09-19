bd6dfa97b664508d65ea59a4ca030fdcd43923de
sarah <sarahkharraz@gmail.com>
2019-09-19 19:37:50 +0100

    Add a grey star but doesnt but add one when its 5 too

--- src/views/helpers/starFinder.js
+++ src/views/helpers/starFinder.js
@@ -1,7 +1,6 @@
 module.exports = number => {
   let starsImages = "";
   let numberOfWholeStars = Math.floor(number);
-  const totalOfStars = 5;
 
   for (let i = 0; i < numberOfWholeStars; i++) {
     starsImages += "<img src='/img/starYellow.svg' class='star'/>";
@@ -11,8 +10,14 @@ module.exports = number => {
     starsImages += "<img src='/img/starHalf.svg' class='star'/>";
   }
 
-  if (starsImages <= totalOfStars) {
+  function numGreyStars() {
+    const starsRoundedDown = Math.floor(number);
+    return 5 - starsRoundedDown;
+  }
+
+  for (let i = 0; i < numGreyStars(); i++) {
     starsImages += "<img src='/img/starGrey.svg' class='star'/>";
   }
+
   return starsImages;
 };
