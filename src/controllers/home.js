695a60165c84977ada2017425652f15399836629
reubengt <reubengeorgethomas@gmail.com>
2019-09-19 16:50:50 +0100

    fix database delete query to only delete the row that matches user's cookie

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,19 +1,25 @@
 const deleteData = require("../model/queries/deleteData");
-
+const jwt = require("jsonwebtoken");
 exports.get = (req, res) => {
   if (req.cookies.meetmecookie) {
-    res.clearCookie("meetmecookie");
+    const jwtToken = req.cookies.meetmecookie;
+    jwt.verify(jwtToken, process.env.SECRET, (err, decodedvalues) => {
+      if (err) {
+        res.status(404).end();
+      } else {
+        deleteData(jwtToken, (error, result) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.render("home", {
+              title: "home",
+              cssPath: "/css/homeStyle.css",
+              jsPath: "/js/home.js"
+            });
+          }
+        });
+        res.clearCookie("meetmecookie");
+      }
+    });
   }
-
-  deleteData((error, result) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.render("home", {
-        title: "home",
-        cssPath: "/css/homeStyle.css",
-        jsPath: "/js/home.js"
-      });
-    }
-  });
 };
