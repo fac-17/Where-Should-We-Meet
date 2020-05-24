ed4f33fcefd556e1ab02880e78c22ea9453d4c9a
reubengt <reubengeorgethomas@gmail.com>
2020-05-24 04:20:49 +0100

    add placeholder image for missing image links

--- src/helpers/venueFilter.js
+++ src/helpers/venueFilter.js
@@ -1,16 +1,18 @@
-const venueFilter = venuesArray => {
-  let newVenueArray = venuesArray.map(venueObject => {
+const venueFilter = (venuesArray) => {
+  let newVenueArray = venuesArray.map((venueObject) => {
     let newVenueObject = {};
     ["name", "image_url", "rating", "price"].forEach(
-      property => (newVenueObject[property] = venueObject[property])
+      (property) => (newVenueObject[property] = venueObject[property])
     );
+    if (!newVenueObject.image_url) {
+      newVenueObject.image_url = "/img/imagenotavailable.jpg";
+    }
     venueObject.location.display_address.pop(); //remove"united kingdom"
     let address = venueObject.location.display_address.join(", ");
     let postcode = venueObject.location.zip_code;
     address = address.replace(/London\s/gi, "").replace(/,[^,]*$/gi, "");
     newVenueObject.address = address;
     newVenueObject.postcode = postcode;
-    console.log(newVenueObject);
     return newVenueObject;
   });
   return newVenueArray;
